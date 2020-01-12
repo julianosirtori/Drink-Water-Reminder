1b207eb76d87ba3216f1e91dce06e6842164bf2b
Juliano Sirtori <julianosirtori@gmail.com>
2020-01-12 18:27:54 -0300

    Add Reactotron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 export default function App() {
